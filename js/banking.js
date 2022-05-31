7c0f10f1a9342757d88317b8834e2ef5f6fa2ad9
nadimahmed1 <nadimarafat26@gmail.com>
2022-05-31 08:05:11 +0600

    update balance

--- js/banking.js
+++ js/banking.js
@@ -18,19 +18,26 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     // clear deposit input field
     depositInputField.value = '';
 
-    // set withdraw
-    document.getElementById('withdraw-button').addEventListener('click', function () {
-        const withdrawInputField = document.getElementById('withdraw-input');
-        const withdrawTotalText = withdrawInputField.value;
-        const previousWithdrawAmount = parseFloat(withdrawTotalText);
-        // 
-        const withdrawTotal = document.getElementById('withdraw-total');
-        const previousWithdrawText = withdrawTotal.innerText;
-        const previousWithdrawTotal = parseFloat(previousWithdrawText);
-        const newWithdrawTotal = previousWithdrawTotal + previousWithdrawAmount;
-        withdrawTotal.innerText = newWithdrawTotal;
-        // clear withdraw inpiut field
-        withdrawInputField.value = '';
 
-    })
 })
+// set withdraw
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawInputField = document.getElementById('withdraw-input');
+    const withdrawTotalText = withdrawInputField.value;
+    const previousWithdrawAmount = parseFloat(withdrawTotalText);
+    // 
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+    const newWithdrawTotal = previousWithdrawTotal + previousWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+    // clear withdraw inpiut field
+    withdrawInputField.value = '';
+    // set withdraw - balance update
+
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotalAmount = parseFloat(previousBalanceTotalText);
+    const newBalanceTotal = previousBalanceTotalAmount - newWithdrawTotal;
+    balanceTotal.innerText = newBalanceTotal;
+}) 
